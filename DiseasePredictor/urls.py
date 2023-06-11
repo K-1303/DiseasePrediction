96628e1f96929d4254cd3e1612ffdb4908c4e80f
Kavya Dua <114342842+K-1303@users.noreply.github.com>
2023-06-12 01:31:13 +0530

    Update urls.py

--- DiseasePredictor/urls.py
+++ DiseasePredictor/urls.py
@@ -3,6 +3,6 @@ from .views import predict, insert_patient_data, train
 
 urlpatterns = [
     path('prediction/<str:symptoms>/', predict),
-    path('insertpd', insert_patient_data),
+    
     path('train', train),
 ]
